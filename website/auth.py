cbdf306ed81fa9e2517475ef10376da30924edcf
Pedro Henrique Maia <pedromaia1218@gmail.com>
2022-07-15 15:29:58 -0300

    Templates concluidas

--- website/auth.py
+++ website/auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
@@ -10,6 +10,6 @@ def login():
 def logout():
     return "Aqui vai ser feito o logout!"
 
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
     return render_template("sign_up.html")
