83f914503f51e0cd19c9bcf9a2a08484cd60c531
Pedro Henrique Maia <pedromaia1218@gmail.com>
2022-08-01 11:48:52 -0300

    Models para QNAS, e database de teste

--- website/models.py
+++ website/models.py
@@ -1,3 +1,5 @@
+from email.policy import default
+from tabnanny import check
 from . import db
 from flask_login import UserMixin
 
@@ -13,3 +15,26 @@ class User(db.Model, UserMixin):
     senha = db.Column(db.String(150))
     nome = db.Column(db.String(150))
     userType = db.Column(db.String(10))
+
+class Qnas(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    sector_id = db.Column(db.Integer, db.ForeignKey('sector.id'))
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    enabled = db.Column(db.Boolean, default=True)
+    check = db.Column(db.Integer, default=0)
+    sent = db.Column(db.Integer, default=0)
+
+class Answers(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    qnas_id = db.Column(db.Integer, db.ForeignKey('qnas.id'))
+    answer = db.Column(db.String(500))
+
+class Questions(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    qnas_id = db.Column(db.Integer, db.ForeignKey('qnas.id'))
+    question = db.Column(db.String(500))
+
+class Contexts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    qnas_id = db.Column(db.Integer, db.ForeignKey('qnas.id'))
+    context = db.Column(db.String(500))
