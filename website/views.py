93fa470b071263ec8a8d0cc9a80ce7bfaf6bc8b6
Pedro Henrique Maia <pedromaia1218@gmail.com>
2022-08-10 09:53:37 -0300

    Cadastro de QnA completo

--- website/views.py
+++ website/views.py
@@ -1,9 +1,40 @@
-from flask import Blueprint, render_template
+from contextvars import Context
+from email import message
+from sqlite3 import enable_shared_cache
+from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
+from .models import Qnas, Answers, Questions, Contexts
+from . import db
 
 views = Blueprint('views', __name__)
 
-@views.route('/')
+@views.route('/', methods=['GET', 'POST'])
 @login_required
 def home():
-    return render_template("home.html", user=current_user)
+    if request.method == 'POST':
+        answer = request.form.get('answer')
+        # print(questions)
+        # print(answer)
+
+        new_qna = Qnas(sector_id=current_user.sector_id, user_id=current_user.id)
+        db.session.add(new_qna)
+        db.session.commit()
+
+        new_answer = Answers(qnas_id=new_qna.id, answer=answer)
+        db.session.add(new_answer)
+
+        for e in request.form.getlist('question'):
+            new_question = Questions(qnas_id=new_qna.id ,question=e)
+            db.session.add(new_question)
+
+        for j in request.form.getlist('context'):
+            new_context = Contexts(qnas_id=new_qna.id ,context=j)
+            db.session.add(new_context)
+            
+        db.session.commit()
+        
+    qnas = Qnas.query.all()
+    questions = Questions.query.all()
+    contexts = Contexts.query.all()
+    answers = Answers.query.all()
+    return render_template("home.html", user=current_user, qnas=qnas, questions=questions, answers=answers, contexts=contexts)
